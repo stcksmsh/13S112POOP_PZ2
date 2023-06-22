3282a19f395a9a224eccb68a44cc06dcdbe0c006
stcksmsh <vukicevickosta@gmail.com>
2023-06-22 19:36:50 +0200

    Updated README.md and JSONParser added line count badge to README implemented JSONParser.save()

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-
 ![Logo](./bin/images/logo.png)
-[![Discord](https://img.shields.io/badge/Discord-stcksmsh-7289DA?style=for-the-badge)](https://discord.com/users/178785875159678977)   [![Linkedin](https://img.shields.io/badge/Linkedin-Kosta_Vukicevic-0072b1?style=for-the-badge)](https://www.linkedin.com/in/kostavukicevic/)   [![Language](https://img.shields.io/badge/Language-Java-f89820?style=for-the-badge)](https://www.java.com/)
+[![Language](https://img.shields.io/badge/Language-Java-f89820?style=for-the-badge)](https://www.java.com/)   ![LineCount](https://img.shields.io/badge/Lines-2k-red?style=for-the-badge)   [![Linkedin](https://img.shields.io/badge/Linkedin-Kosta_Vukicevic-0072b1?style=for-the-badge)](https://www.linkedin.com/in/kostavukicevic/)   [![Discord](https://img.shields.io/badge/Discord-stcksmsh-7289DA?style=for-the-badge)](https://discord.com/users/178785875159678977)
+
 # Open source spreadsheet program made with Java AWT
+
 ## Description
+
 A simple spreadsheet program made using (mostly) Java's AWT package for the GUI. It was a project for a second year CS class, but I got a little carried away and just continued adding features to it.
+
 ## Author
+
 - [@stcksmsh](https://www.github.com/stcksmsh)
+
 <!-- ## Demo 
     TODO
   -->
+
 ## Features
+
 - Multiple sheets
 - Cell references
 - CSV and JSON save formats
--- src/Table/JSONParser.java
+++ src/Table/JSONParser.java
@@ -1,15 +1,51 @@
 package Table;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.AbstractMap;
+
 public class JSONParser extends Parser {
     @Override
     public int save(Table table, String filename) {
-        // TODO Auto-generated method stub
+        try {
+            FileWriter file = new FileWriter(filename);
+            BufferedWriter bw = new BufferedWriter(file);
+            String currentSheetName = null;
+            bw.write("{\"table\":{");
+            for (AbstractMap.SimpleEntry<String, Cell> entry : table) {
+                Cell cell = entry.getValue();
+                String sheetName = entry.getKey();
+                String column = cell.getCellIdentifier().getColumn();
+                int row = cell.getCellIdentifier().getRow();
+                String value = cell.getValue();
+                String formatCode = cell.getFormatCode();
+                if (value.equals("") && formatCode.equals("T")) {
+                    continue;
+                }
+                if (currentSheetName == null) {
+                    currentSheetName = sheetName;
+                    bw.write(String.format("\"%s\":[", currentSheetName));
+                } else if (!currentSheetName.equals(sheetName)) {
+                    bw.write(String.format("],\"%s\":[", currentSheetName));
+                } else {
+                    bw.write(",");
+                }
+
+                bw.write(String.format("{\"row\":\"%s\",\"column\":%d,\"value\":\"%s\",\"formatCode\":\"%s\"}", column,
+                        row, value, formatCode));
+            }
+            bw.write("]}}");
+            bw.close();
+            file.close();
+        } catch (IOException IOe) {
+            return -1;
+        }
         return 0;
     }
 
     @Override
     public Table open(String filename) {
-        // TODO Auto-generated method stub
         return null;
     }
 }
